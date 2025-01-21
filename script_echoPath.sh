9617a3d8cbccf5dbbbc460ed67ae8ee3f92c3e92
changwoo <branden.c.w.kang@gmail.com>
2025-01-22 00:33:11 +0900

    update_shFile

--- script_echoPath.sh
+++ script_echoPath.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "==========================================================================================================="
+
+echo "NYU_PREPROCESS: $NYU_PREPROCESS"
+echo "NYU_ROOT: $NYU_ROOT"
+echo "NYU_LOG: $NYU_LOG"
+
 echo "KITTI_PREPROCESS: $KITTI_PREPROCESS"
 echo "KITTI_ROOT: $KITTI_ROOT"
 echo "KITTI_LOG: $KITTI_LOG"
 
 echo "MONOSCENE_OUTPUT: $MONOSCENE_OUTPUT"
+
+echo "==========================================================================================================="
+echo ""
